1b3886b0b5c59edea681bd9bc3a7385b598987cd
Dom58 <dndahimana58@gmail.com>
2022-05-16 18:34:34 +0200

    Single routes file created and added all routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
-import React, { useState } from 'react';
-import { Route, Routes, BrowserRouter} from "react-router-dom";
-import Home from './pages/home';
-import AllRooms from './pages/rooms/rooms';
-import SingleRoom from './pages/rooms/room';
-import { Room } from './pages/rooms/rooms';
-
+import React from 'react';
+import { BrowserRouter} from "react-router-dom";
+import AllRoutes from './routes';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/"  element={<Home />} />
-          <Route path="/rooms" element={<AllRooms />} />
-          <Route path="/rooms/:slug" element={<SingleRoom />} />
-        </Routes>
+          <AllRoutes />
       </BrowserRouter>
     </div>
   );
---
+++ src/routes/index.tsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import Home from '../pages/home';
+import AllRooms from '../pages/rooms/rooms';
+import SingleRoom from '../pages/rooms/room';
+
+const AllRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/"  element={<Home />} />
+      <Route path="/rooms" element={<AllRooms />} />
+      <Route path="/rooms/:slug" element={<SingleRoom />} />
+    </Routes>
+  )
+}
+export default AllRoutes;
